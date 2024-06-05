7079da1c39f0d49db91167185f6408259b5c4f52
MVPee <mariusvanpee@hotmail.be>
2024-06-05 21:24:15 +0200

    opti map (rm useless 1 and 0)

--- srcs/cub3d/parsing/check_map.c
+++ srcs/cub3d/parsing/check_map.c
@@ -6,11 +6,122 @@
 /*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 10:37:05 by mvpee             #+#    #+#             */
-/*   Updated: 2024/06/05 09:53:57 by mvpee            ###   ########.fr       */
+/*   Updated: 2024/06/05 21:19:14 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
+
+static void get_new_map(char **map)
+{
+    int y;
+    int x;
+    int flag;
+    int n[2];
+
+    // Get N position
+    y = -1;
+    while (map[++y])
+    {
+        x = -1;
+        while (map[y][++x])
+        {
+            if (map[y][x] == 'N')
+            {
+                n[0] = y;
+                n[1] = x;
+            }
+        }
+    }
+
+    // Get all position possible
+    while (flag)
+    {
+        flag = 0;
+        y = -1;
+        while (map[++y])
+        {
+            x = -1;
+            while (map[y][++x])
+            {
+                if (map[y][x] == 'N')
+                {
+                    if (map[y - 1][x] == '0')
+                    {
+                        map[y - 1][x] = 'N';
+                        flag = 1;
+                    }
+                    if (map[y + 1][x] == '0')
+                    {
+                        map[y + 1][x] = 'N';
+                        flag = 1;
+                    }
+                    if (map[y][x - 1] == '0')
+                    {
+                        map[y][x - 1] = 'N';
+                        flag = 1;
+                    }
+                    if (map[y][x + 1] == '0')
+                    {
+                        map[y][x + 1] = 'N';
+                        flag = 1;
+                    }
+                }
+            }
+        }
+    }
+    
+    // Delete all 0 useless and 1
+    y = -1;
+    while (map[++y])
+    {
+        x = -1;
+        while (map[y][++x])
+        {
+            if (map[y][x] == '0' || map[y][x] == '1')
+                map[y][x] = ' ';
+        }
+    }
+
+    // Put N in is initial position
+    y = -1;
+    while (map[++y])
+    {
+        x = -1;
+        while (map[y][++x])
+        {
+            if (map[y][x] == 'N')
+                map[y][x] = '0';
+        }
+    }
+    map[n[0]][n[1]] = 'N';
+
+    // Put border
+    y = -1;
+    while (map[++y])
+    {
+        x = -1;
+        while (map[y][++x])
+        {
+            if (map[y][x] == '0')
+            {
+                if (map[y - 1][x] == ' ')
+                    map[y - 1][x] = '1';
+                if (map[y + 1][x] == ' ')
+                    map[y + 1][x] = '1';
+                if (map[y][x - 1] == ' ')
+                    map[y][x - 1] = '1';
+                if (map[y][x + 1] == ' ')
+                    map[y][x + 1] = '1';
+            }
+        }
+    }
+
+    // Display
+    int i = -1;
+    while (++i < ft_splitlen((const char **)map))
+        ft_printf("%d\t%s\n", i, map[i]);
+}
 
 bool check_map(char **map)
 {
@@ -47,5 +158,6 @@ bool check_map(char **map)
         return (ft_printf_fd(2, "Error\nNo player online\n"), true);
     else if (count > 1)
         return (ft_printf_fd(2, "Error\nIt's not a multiplayer game\n"), true);
+    get_new_map(map);
     return (false);
 }
