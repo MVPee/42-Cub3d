ec1936811145097fb355db0083b9f16a9575eac5
MVPee <mariusvanpee@hotmail.be>
2024-06-06 11:13:46 +0200

    norm in includes

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -2,9 +2,12 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mvan-pee <mvan-pee@student.s19.be>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+
+	+:+     */
+/*   By: mvan-pee <mvan-pee@student.s19.be>         +#+  +:+
+	+#+        */
+/*                                                +#+#+#+#+#+
+	+#+           */
 /*   Created: 2024/04/15 10:06:22 by mvan-pee          #+#    #+#             */
 /*   Updated: 2024/04/15 10:06:22 by mvan-pee         ###   ########.fr       */
 /*                                                                            */
@@ -14,28 +17,26 @@
 # define CUB3D_H
 
 # include "../srcs/libft/includes/libft.h"
-# include <stdio.h>
 # include <errno.h>
 # include <stdbool.h>
+# include <stdio.h>
 
 typedef struct s_data
 {
-    char *north;
-    char *south;
-    char *west;
-    char *east;
-
-    int *floor;
-    int *ceiling;
-
-    char **file;
-    char **map;
-}   t_data;
+	char	*north;
+	char	*south;
+	char	*west;
+	char	*east;
+	int		*floor;
+	int		*ceiling;
+	char	**file;
+	char	**map;
+}	t_data;
 
 // PARSING
-bool check_extension(char *str);
-bool check_file(t_data *data);
-bool check_map(char **map);
-void get_new_map(char **map);
+bool	check_extension(char *str);
+bool	check_file(t_data *data);
+bool	check_map(char **map);
+void	get_new_map(char **map);
 
 #endif
