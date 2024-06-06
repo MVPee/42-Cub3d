04599778892749bcfeb32460ec8f8be20768ba82
noureh10 <noureh.it@outlook.com>
2024-06-06 14:51:35 +0200

    misc

--- srcs/cub3d/map_rendering/raycasting.c
+++ srcs/cub3d/map_rendering/raycasting.c
@@ -6,11 +6,11 @@
 /*   By: nechaara <nechaara@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:22:06 by nechaara          #+#    #+#             */
-/*   Updated: 2024/06/05 14:50:02 by nechaara         ###   ########.fr       */
+/*   Updated: 2024/06/06 13:17:03 by nechaara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/cub3d.h"
+#include "../../../includes/cub3d.h"
 
 void	compute_and_display(char **map, t_pos player, size_t main_ray_amplitude)
 {
