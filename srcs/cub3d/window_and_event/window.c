5ac6d2ef2f7559489fa66e97bf21e196871a06fd
nechaara <nour.eha23@gmail.com>
2024-06-06 14:43:08 +0200

    Adding windows init function and working on game loop function

--- srcs/cub3d/window_and_event/window.c
+++ srcs/cub3d/window_and_event/window.c
@@ -6,7 +6,45 @@
 /*   By: nechaara <nechaara@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:50:46 by nechaara          #+#    #+#             */
-/*   Updated: 2024/06/05 14:50:47 by nechaara         ###   ########.fr       */
+/*   Updated: 2024/06/06 14:20:36 by nechaara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../../includes/cub3d.h"
+
+static bool	window_init(t_data *data)
+{
+	mlx_t		*mlx;
+	mlx_image_t	*image;
+
+	mlx = mlx_init(WIDTH, HEIGHT, PROGRAM_NAME, true);
+	if (!mlx)
+	{
+		mlx_strerror(mlx_errno);
+		return (false);
+	}
+	image = mlx_new_image(mlx, WIDTH, HEIGHT);
+	if (!image)
+	{
+		mlx_terminate(mlx);
+		mlx_strerror(mlx_errno);
+		return (false);
+	}
+	data->mlx = mlx;
+	data->image = image;
+	return (true);
+}
+
+int	game_loop(t_data *data)
+{
+	t_pos	*player_pos;
+	
+	if (!data)
+		return (EXIT_FAILURE);
+	else if(!window_init(data))
+		return (error_handler(WIN_INIT_FAILED));
+	mlx_key_hook(data->mlx, move_keyhook, player_pos);
+	mlx_loop(data->mlx);
+	mlx_terminate(data->mlx);
+	return (EXIT_SUCCESS);
+}
