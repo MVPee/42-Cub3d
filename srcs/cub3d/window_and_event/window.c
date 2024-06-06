577aee267864c9ef8d80a159fe78bbc14d5b0f14
noureh10 <noureh.it@outlook.com>
2024-06-06 15:00:47 +0200

    fix segv

--- srcs/cub3d/window_and_event/window.c
+++ srcs/cub3d/window_and_event/window.c
@@ -6,7 +6,7 @@
 /*   By: nechaara <nechaara@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:50:46 by nechaara          #+#    #+#             */
-/*   Updated: 2024/06/06 14:20:36 by nechaara         ###   ########.fr       */
+/*   Updated: 2024/06/06 14:59:44 by nechaara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,13 @@ static bool	window_init(t_data *data)
 
 int	game_loop(t_data *data)
 {
-	t_pos	*player_pos;
+	t_pos	player_pos;
 	
 	if (!data)
 		return (EXIT_FAILURE);
 	else if(!window_init(data))
 		return (error_handler(WIN_INIT_FAILED));
-	mlx_key_hook(data->mlx, move_keyhook, player_pos);
+	mlx_key_hook(data->mlx, move_keyhook, &player_pos);
 	mlx_loop(data->mlx);
 	mlx_terminate(data->mlx);
 	return (EXIT_SUCCESS);
