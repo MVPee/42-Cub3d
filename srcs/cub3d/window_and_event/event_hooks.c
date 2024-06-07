a2036dd555138dc3fb6c63e4c75a4ba716ee8b57
mvpee <mariusvanpee@hotmail.be>
2024-06-07 16:12:21 +0200

    add event hooks

--- srcs/cub3d/window_and_event/event_hooks.c
+++ srcs/cub3d/window_and_event/event_hooks.c
@@ -3,29 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   event_hooks.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nechaara <nechaara@student.s19.be>         +#+  +:+       +#+        */
+/*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:50:53 by nechaara          #+#    #+#             */
-/*   Updated: 2024/06/06 14:37:03 by nechaara         ###   ########.fr       */
+/*   Updated: 2024/06/07 16:12:10 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/cub3d.h"
 
-void move_keyhook(mlx_key_data_t keydata, void *param)
+void    rotate_player(t_data *data, uint16_t keycode)
 {
-	t_pos	*current_pos;
-	
-	if (!param)
-		return ;
-	current_pos = (t_pos *) param;
-	if (keydata.key == MLX_KEY_Z && keydata.action == (MLX_REPEAT))
-		current_pos->y += 0.5f;
-	if (keydata.key == MLX_KEY_S && keydata.action == (MLX_REPEAT))
-		current_pos->y -= 0.5f;
-	if (keydata.key == MLX_KEY_Q && keydata.action == (MLX_REPEAT))
-		current_pos->x -= 0.5f;
-    if (keydata.key == MLX_KEY_D && keydata.action == (MLX_REPEAT))
-		current_pos->x += 0.5f;
-	printf("current_pos x : %lf\n current_pos y %lf\n",current_pos->x, current_pos->y);
+    
+}
+
+void    move_player(t_data *data, uint16_t keycode)
+{
+    
+}
+
+void    move_keyhook(mlx_key_data_t keydata, void *param)
+{
+    t_data * working_data;
+    
+    if (!param)
+        return ;
+    working_data = (t_data *) param;
+    printf("keycode %d\n", keydata.key);
+    /*
+    if (keydata.key == MLX_KEY_Z && (keydata.action == MLX_REPEAT
+        || keydata.action == (MLX_PRESS)))
+        working_data.p->y += 0.5f;
+    else if (keydata.key == MLX_KEY_S && (keydata.action == (MLX_REPEAT)
+        || keydata.action == (MLX_PRESS)))
+        current_pos->y -= 0.5f;
+    else if (keydata.key == MLX_KEY_Q && (keydata.action == (MLX_REPEAT)
+        || keydata.action == (MLX_PRESS)))
+        current_pos->x -= 0.5f;
+    else if (keydata.key == MLX_KEY_D && (keydata.action == (MLX_REPEAT)
+        || keydata.action == (MLX_PRESS)))
+        current_pos->x += 0.5f;
+    */
+    //printf("current_pos x : %lf\n current_pos y %lf\n",current_pos->x, current_pos->y);
 }
